72348064d31a82d5f807523ab3c7110198d97dc0
Shifana28farveen <shifanafarveen.sightspectrum@gmail.com>
2023-09-19 18:09:52 +0530

    first commit

---
+++ list/19-09-2023.py
@@ -0,0 +1,50 @@
+List = ["c", "c++", "python", "java"]
+print(List[2])
+print(List[0])
+print(List[-1])
+print(List[-4])
+List.append("C#")
+print(List)
+List1= List.copy()
+print(List1)
+print(List.count("java"))
+List2 = ["html", "javascript"]
+List.extend(List2)
+print(List)
+print(List.index("java"))
+List.insert(4,"CSS")
+print(List)
+List.pop()
+print(List)
+List.remove("c++")
+print(List)
+List = ["c", "c++", "python", "java"]
+List.reverse()
+print(List)
+List.sort()
+print(List)
+
+a = ["a","b","c","d","e","f"]
+a.remove("b")
+print(a)
+a = ["reverse","earth","pluto"]
+a.reverse()
+print(a)
+a = [1,2,3,5,["a","b","c"],91]
+a_list= ["a","b","c"]
+print(a_list[1:])
+
+a = [1,2,3,5,["a","b","c"],91]
+print(a[4][1:])
+
+a = "Hello World"
+print(a[::2])
+print(a[::-1])
+print(a[::-2])
+print(a[::3])
+
+
+a = "This is a company"
+print(a. split(" "))
+
+
